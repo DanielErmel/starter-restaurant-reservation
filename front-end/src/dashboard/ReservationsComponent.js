3a51d9f226413e7d7453bd9b14cdab4f36fcde7e
DanielErmel <ermel.daniel.s@gmail.com>
2023-01-04 16:36:48 -0600

    reverted to <a>

--- front-end/src/dashboard/ReservationsComponent.js
+++ front-end/src/dashboard/ReservationsComponent.js
@@ -1,6 +1,5 @@
 import { updateResStatus } from "../utils/api"
-import * as React from "react";
-import { Link } from "react-router-dom";
+
 export default function ReservationsComponent({reservations, loadDashboard}){
 
   function onCancel(e,reservation){
@@ -27,15 +26,12 @@ export default function ReservationsComponent({reservations, loadDashboard}){
              <p className="card-text" data-reservation-id-status={reservation.reservation_id}>Reservation Status: {reservation.status}</p>
              
              { reservation.status!=='seated' ? 
-             <Link to={`/reservations/${reservation.reservation_id}/seat`}>Seat</Link> : null }
-
-             {/* { reservation.status!=='seated' ? 
              <a href={`/reservations/${reservation.reservation_id}/seat`}>
-             <button className="btn btn-primary w-25 mb-1 ml-1" type="button">Seat</button></a> : null } */}
-              <Link to={`/reservations/${reservation.reservation_id}/edit`}>Edit</Link>
-             {/* <a href={`/reservations/${reservation.reservation_id}/edit`}>
+             <button className="btn btn-primary w-25 mb-1 ml-1" type="button">Seat</button></a> : null }
+
+             <a href={`/reservations/${reservation.reservation_id}/edit`}>
             <button className="btn btn-secondary w-25 mb-1 ml-1" type="button">Edit</button>
-            </a> */}
+            </a>
 
            {reservation.status !== "cancelled" &&  <button type="button" className="btn btn-danger w-25 mb-1 ml-1" data-reservation-id-cancel={reservation.reservation_id} 
             onClick={(e)=>onCancel(e,reservation)}>Cancel</button> }
